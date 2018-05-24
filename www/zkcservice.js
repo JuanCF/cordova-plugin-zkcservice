301b96d005cc282fa7ac6f2f629973383bd38e90
matsosos <39056377+matsosos@users.noreply.github.com>
2018-05-24 16:27:57 +0200

    correction of code

--- www/zkcservice.js
+++ www/zkcservice.js
@@ -3,17 +3,17 @@ var exec = cordova.require('cordova/exec');
 var service = "ZKCService";
 
 exports.Servicebind = function(args, success, error) {
-	exec(win, fail, service, "Servicebind", [args]);
+	exec(success, error, service, "Servicebind", [args]);
 };
 
 exports.onDataReceived = function(handle, success, error) {
-	exec(win, fail, service, "onDataReceived", [handle]);
+	exec(success, error, service, "onDataReceived", [handle]);
 };
 
 exports.onError = function(handle, success, error) {
-	exec(win, fail, service, "onError", [handle]);
+	exec(success, error, service, "onError", [handle]);
 };
 
 exports.sendData = function(handle, data, success, error) {
-	exec(win, fail, service, "sendData", [handle, data]);
+	exec(success, error, service, "sendData", [handle, data]);
 };
